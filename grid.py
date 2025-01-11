7cc5853207bfea266f15347a54be8592c5b522b1
alexiatsikri <tsikrialexia@gmail.com>
2025-01-11 23:25:15 +0200

    Creating the grid

---
+++ grid.py
@@ -0,0 +1,36 @@
+import pygame
+
+class Grid:
+    def __init__(self):
+        self.num_rows=20
+        self.num_cols=10
+        self.cell_size=30
+        self.grid = [[0 for j in range(self.num_cols)] for i in range(self.num_rows)]
+        self.colors = self.get_cell_colors() #list to hold all the colors
+
+    def print_grid(self):
+        for row in range(self.num_rows):
+            for column in range(self.num_cols):
+                print(self.grid[row][column],end = " ")  #to be able to see if the grid is ok
+            print() 
+
+    def get_cell_colors(self):
+
+        dark_grey = (26,31,40)
+        green = (47,230,23)
+        red = (232, 18,18)
+        orange = (226, 116, 17)
+        yellow = (237, 234, 4)
+        purple = (166, 0, 247)
+        cyan = (21, 204, 209)
+        blue = (13, 64, 216)
+
+        return [dark_grey, green, red, orange, yellow, purple, cyan, blue]
+    
+    #draw metod
+    def draw(self,screen):
+        for row in range(self.num_rows):
+            for column in range(self.num_cols):
+                cell_value = self.grid[row][column]
+                cell_rect = pygame.Rect(column *self.cell_size+1, row*self.cell_size+1, self.cell_size-1, self.cell_size-1)
+                pygame.draw.rect(screen, self.colors[cell_value], cell_rect) # surface, color, rect
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pygame
+import pygame,sys
+from grid import Grid
 
 
 pygame.init()
@@ -9,11 +10,21 @@ dark_blue = (44, 44, 127)
 # Basic set up
 
 # set up display surface
-screen = pygame.display.set_mode((600,600))
+screen = pygame.display.set_mode((300,600))
 pygame.display.set_caption("Python Tetris")
 
 # set up frame rate of the game
 clock = pygame.time.Clock()
+
+#set up the grid
+game_grid = Grid()
+
+#test to check colors and grids
+game_grid.grid[0][0]=1
+game_grid.grid[3][5]=4
+game_grid.grid[17][8]=7
+
+game_grid.print_grid()
 
 while True:
     for event in pygame.event.get():
@@ -23,6 +34,7 @@ while True:
     
     # Drawing
     screen.fill(dark_blue)
+    game_grid.draw(screen)
 
     pygame.display.update()
     clock.tick(60)
